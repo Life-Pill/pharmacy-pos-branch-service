a57595f428d3ad2c6c4e6e6c1b45cdc0cb7a22c5
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-03-08 21:29:52 +0530

    initializing branch service

---
+++ src/main/java/com/lifePill/posbranchservice/PosBranchServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lifePill.posbranchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosBranchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosBranchServiceApplication.class, args);
+	}
+
+}
